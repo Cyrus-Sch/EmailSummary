2bb5690e6ea2160f282b5f7b0596210b0024eac9
cyrusscholten <cyrusscholten@gmail.com>
2023-03-20 22:59:49 +0700

    added html and proc

--- get_gmail.py
+++ get_gmail.py
@@ -34,7 +34,7 @@ def get_auth_url():
     flow = InstalledAppFlow.from_client_secrets_file(
         'credentials.json',
         SCOPES,
-        redirect_uri=f"https://emailsummary.herokuapp.com/oauth2callback/"
+        redirect_uri="https://emailsummary.herokuapp.com/oauth2callback/"
     )
 
     auth_url, _ = flow.authorization_url(access_type='offline', prompt='consent')
