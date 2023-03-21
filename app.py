3430ca573f5b04f1b0b3411dd6fd902ee6657650
cyrusscholten <cyrusscholten@gmail.com>
2023-03-21 16:17:32 +0700

    added workers

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def background_get_summary(credentials_txt_obj, user_id):
         print(f"Queuing for User {user_id}")
         print(q.fetch_job(job_identification))
         job = q.fetch_job(job_identification)
-        if job.get_status() is "finished" or job.get_status() is "failed":
+        if job.get_status() == "finished" or job.get_status() == "failed":
             job.delete()
         if job is None:
             print("Starting....")
